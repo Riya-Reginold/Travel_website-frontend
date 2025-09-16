6be6268d151a883b17d6cf4ddd85fbf069b20b49
Riya Reginold Kalluvalappil <riyareginoldkalluvalappil@gmail.com>
2025-09-16 20:19:33 +0200

    updated

--- components/Home/HeroSection/Hero.tsx
+++ components/Home/HeroSection/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className='relative w-full h-[120vh] sm:h-[100vh]'>
         <div className='absolute top-0 left-0 w-full h-full bg-gray-800 opacity-70'>  </div>
-        <video src="/images/bg_video.mp4" autoPlay muted loop preload='metadata' className='w-full h-full object-cover'/>    
+        <video src="./images/bg_video.mp4" autoPlay muted loop preload='metadata' className='w-full h-full object-cover'/>    
         <div className='absolute z-[100] w-full h-full top-[50%]  left-[50%] translate-x-[-50%] translate-y-[-50%]'>
           <div className='flex items-center justify-center flex-col w-full h-full'>
             <div>
