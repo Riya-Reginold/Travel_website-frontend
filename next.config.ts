ab6d22596d3a144c4a4949de24a996c904ca87cc
Riya Reginold Kalluvalappil <riyareginoldkalluvalappil@gmail.com>
2025-09-16 20:30:04 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output:'export',
+  assetPrefix: "/Travel_website-frontend/",
+  basePath: "/Travel_website-frontend"
 };
 
 export default nextConfig;
